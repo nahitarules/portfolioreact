332076501af269b25e0339339cad564260ce1eda
nahome <nahom.teshome@yahoo.com>
2019-08-25 20:58:12 -0700

    Updates

---
+++ precache-manifest.f4fc304761e712b6aa78dbf29c3517f8.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "12e81f23dfb5b83fc1a2df5a8a2f0bd5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "60d59a7b04b0a5592053",
+    "url": "/static/css/2.60a6b8e0.chunk.css"
+  },
+  {
+    "revision": "a991ebbb8e842a898c2c",
+    "url": "/static/css/main.0a4d10c5.chunk.css"
+  },
+  {
+    "revision": "60d59a7b04b0a5592053",
+    "url": "/static/js/2.080fa48e.chunk.js"
+  },
+  {
+    "revision": "a991ebbb8e842a898c2c",
+    "url": "/static/js/main.065f5046.chunk.js"
+  },
+  {
+    "revision": "4ea64ac89cd6ac4aa8d1",
+    "url": "/static/js/runtime~main.9af7159c.js"
+  },
+  {
+    "revision": "aa0ac6698b8d82c2b76ee936769965b7",
+    "url": "/static/media/glassMe.aa0ac669.jpg"
+  },
+  {
+    "revision": "4c241b8e72a1ace5db37a0eef013eddb",
+    "url": "/static/media/me.4c241b8e.jpg"
+  }
+]);
