6487cc29f8660b7beaee1c3219759797db974319
nahome <nahom.teshome@yahoo.com>
2019-08-25 21:58:39 -0700

    Updates

---
+++ precache-manifest.652bb27cf495382abacc0361b4d62a48.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8ce3c7d437e6d752ddae363c86a4ece2",
+    "url": "/portfolioreact/index.html"
+  },
+  {
+    "revision": "60d59a7b04b0a5592053",
+    "url": "/portfolioreact/static/css/2.60a6b8e0.chunk.css"
+  },
+  {
+    "revision": "77955bd0eef9ae9778d4",
+    "url": "/portfolioreact/static/css/main.0a4d10c5.chunk.css"
+  },
+  {
+    "revision": "60d59a7b04b0a5592053",
+    "url": "/portfolioreact/static/js/2.080fa48e.chunk.js"
+  },
+  {
+    "revision": "77955bd0eef9ae9778d4",
+    "url": "/portfolioreact/static/js/main.a483c796.chunk.js"
+  },
+  {
+    "revision": "40b3ceda5274dad0f224",
+    "url": "/portfolioreact/static/js/runtime~main.4247a9be.js"
+  },
+  {
+    "revision": "aa0ac6698b8d82c2b76ee936769965b7",
+    "url": "/portfolioreact/static/media/glassMe.aa0ac669.jpg"
+  },
+  {
+    "revision": "4c241b8e72a1ace5db37a0eef013eddb",
+    "url": "/portfolioreact/static/media/me.4c241b8e.jpg"
+  }
+]);
