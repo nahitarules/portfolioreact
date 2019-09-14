b0d052b801a750033a857daf9d0e9a848860dec7
nahome <nahom.teshome@yahoo.com>
2019-09-13 20:34:48 -0700

    projects update

--- src/components/products.js
+++ src/components/products.js
@@ -14,29 +14,80 @@ export default class Products extends Component {
                 return (
                     
                     <div className="projects-grid">
+
+                        
+                        {/* Project 1 */}
+                            <Card shadow={5} style={{minWidth: '450', margin: 'auto'}}> 
+                                <CardTitle style={{color: '#fff', height: '176px', background: 'url(https://bs-uploads.toptal.io/blackfish-uploads/blog/post/seo/og_image_file/og_image/16097/react-context-api-4929b3703a1a7082d99b53eb1bbfc31f.png) center / cover' }}>Bloc Jams React Project</CardTitle>
+                                <CardText>
+                                    Spotify like web app Built in React
+                                </CardText>
+                                <CardActions border>
+                                    <Button colored ><a href="https://github.com/nahitarules/bloc-jams-react" target="_blank">Github</a></Button>
+                                </CardActions>
+                                <CardMenu style={{color: '#fff'}}>
+                                    <IconButton name="share" />
+                                </CardMenu>
+                            </Card>
+                        
+                    </div>
+
+
+                    
+                )
+            } else if(this.state.activeTab === 1) {
+                return (
+                    <div className="projects-grid">
                     {/* Project 1 */}
                         <Card shadow={5} style={{minWidth: '450', margin: 'auto'}}> 
-                            <CardTitle style={{color: '#fff', height: '176px', background: 'url(https://avatarfiles.alphacoders.com/118/118618.jpg) center / cover' }}>React Project</CardTitle>
+                            <CardTitle style={{color: '#fff', height: '176px', background: 'url(https://cdn.auth0.com/blog/illustrations/angular.png) center / cover' }}>Bloc Jams Angular</CardTitle>
                             <CardText>
-                                Spotify like web app Built in React
+                                Spotify like web app Built in Angular
                             </CardText>
                             <CardActions border>
-                                <Button colored>Github</Button>
+                            <Button colored ><a href="https://github.com/nahitarules/my-bloc-jams-angularjs" target="_blank">Github</a></Button>
                             </CardActions>
                             <CardMenu style={{color: '#fff'}}>
                                 <IconButton name="share" />
                             </CardMenu>
                         </Card>
+
+                        
                     </div>
-                    
-                )
-            } else if(this.state.activeTab === 1) {
-                return (
-                    <div><h1>This is Angular</h1></div>
                 )
             } else if(this.state.activeTab === 2) {
                 return (
-                    <div><h1>This is Ruby on Rails</h1></div>
+                    <div className="projects-grid">
+                    {/* Project 1 */}
+                        <Card shadow={5} style={{minWidth: '450', margin: 'auto'}}> 
+                            <CardTitle style={{color: '#fff', height: '176px', background: 'url(https://s3.amazonaws.com/media.skillcrush.com/skillcrush/wp-content/uploads/2015/01/Blog_About-Ruby2.jpg) center / cover' }}>Blocipedia</CardTitle>
+                            <CardText>
+                             Web app that allows users to create wikis built in Ruby on Rails
+                            </CardText>
+                            <CardActions border>
+                            <Button colored ><a href="https://github.com/nahitarules/blocipedia" target="_blank">Github</a></Button>
+                            </CardActions>
+                            <CardMenu style={{color: '#fff'}}>
+                                <IconButton name="share" />
+                            </CardMenu>
+                        </Card>
+
+                        {/* Project 2 */}
+                        <Card shadow={5} style={{minWidth: '450', margin: 'auto'}}> 
+                            <CardTitle style={{color: '#fff', height: '176px', background: 'url(https://s3.amazonaws.com/media.skillcrush.com/skillcrush/wp-content/uploads/2015/01/Blog_About-Ruby2.jpg) center / cover' }}>Open to do</CardTitle>
+                            <CardText>
+                            Open to do is an externally usable API for a basic to-do list application
+                            </CardText>
+                            <CardActions border>
+                            <Button colored ><a href="https://github.com/nahitarules/open-to-do" target="_blank">Github</a></Button>
+                            </CardActions>
+                            <CardMenu style={{color: '#fff'}}>
+                                <IconButton name="share" />
+                            </CardMenu>
+                        </Card>
+
+                        
+                    </div>
                 )
             }
         }
